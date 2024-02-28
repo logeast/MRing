fb658e333993c5e59734e5b04b86d81861fbd2e2
logeast <logeast@outlook.com>
2024-02-28 10:31:49 +0800

    Initial Commit

---
+++ MRing/MRingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MRingApp.swift
+//  MRing
+//
+//  Created by logeast on 2024/2/28.
+//
+
+import SwiftUI
+
+@main
+struct MRingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
